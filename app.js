911691b4c7e7f3d18480a62ca0e32956529e5658
Christian Arredondo <christianarredondo@utexas.edu>
2017-06-15 18:10:28 -0500

    Will it render? Find out, on the next episode..

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cPicks;
 function initMap() {
   // Get JSON data
   $.getJSON('locations.json', function(response) {
-      console.log(response);
       cPicks=response;
+      console.log(cPicks);
   });
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
