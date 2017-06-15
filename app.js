ba1601dfe563284bd49814803e0fc8719763fa61
Christian Arredondo <christianarredondo@utexas.edu>
2017-06-15 18:04:34 -0500

    Try rendering new JSON data in map

--- app.js
+++ app.js
@@ -14,21 +14,21 @@ function initMap() {
     zoom: 11
   });
   // // Locations, in this case some Houston restaurants
-  var locations = [
-    {title:'Hugos', location: {lat: 29.743061, lng: -95.399653}},
-    {title:'Ruggles Green', location: {lat: 29.780465, lng: -95.542325}},
-    {title:'Pluckers', location: {lat: 29.773843, lng: -95.402731}},
-    {title:'Hobbit Cafe', location: {lat: 29.733456, lng: -95.413941}}
-  ];
+  // var locations = [
+  //   {title:'Hugos', location: {lat: 29.743061, lng: -95.399653}},
+  //   {title:'Ruggles Green', location: {lat: 29.780465, lng: -95.542325}},
+  //   {title:'Pluckers', location: {lat: 29.773843, lng: -95.402731}},
+  //   {title:'Hobbit Cafe', location: {lat: 29.733456, lng: -95.413941}}
+  // ];
 
   var largeInfoWindow = new google.maps.InfoWindow();
   var bounds = new google.maps.LatLngBounds();
 
   // Use location array to create an array of markers
-  for (var i =0; i < locations.length; i++) {
+  for (var i =0; i < cPicks.length; i++) {
     // Get position from location array
-    var position = locations[i].location;
-    var title = locations[i].title;
+    var position = cPicks[i].location;
+    var title = locations[i].name;
     // Create a marker for each location
     var marker = new google.maps.Marker({
       map: map,
