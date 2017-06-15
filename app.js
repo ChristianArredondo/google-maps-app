425ca4bb05aa9d8d33f2e9a2e5bf8c3ae52ca7c6
Christian Arredondo <christianarredondo@utexas.edu>
2017-06-15 18:25:24 -0500

    Map is showing up again, now to get back to the JSON data not rendering...

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var map;
 var markers = [];
 var cPicks;
-
-function initMap() {
-  // Get JSON data
+ // Get JSON data
   $.getJSON('locations.json', function(response) {
       cPicks=response;
       console.log(cPicks);
   });
+  
+function initMap() {
+
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 29.751560, lng: -95.363282},
