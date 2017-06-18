0b9dd3c030930003e054ae95c69893249bae42d1
Christian Arredondo <christianarredondo@utexas.edu>
2017-06-17 22:06:19 -0500

    Add function to popular sidebar on click

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ function initMap() {
     zoom: 12
   });
 
-
   // Use AJAX to pull location JSON
   $.getJSON('locations.json', function(response) {
       cPicks=response;
@@ -22,7 +21,7 @@ function initMap() {
       var largeInfoWindow = new google.maps.InfoWindow();
       var bounds = new google.maps.LatLngBounds();
 
-      // Use location array to create an array of markers
+      // Use location array to create markers and popular sidebar
       for (var i =0; i < length; i++) {
         var position = cPicks[i].location;
         var title = cPicks[i].name;
@@ -38,23 +37,44 @@ function initMap() {
         markers.push(marker);
         // Create onClick event to open infowindow at each marker
         marker.addListener('click', function() {
-          populateInfoWindow(this, largeInfoWindow);
+          // Push location data to sidebar
+          for (var i in cPicks) {
+            if (this.title === cPicks[i].name) {
+              titleHTML = '<h2>' + cPicks[i].name + '</h2>';
+              addressHTML = '<p><strong>Address: </strong>' + cPicks[i].address.street + ' ' + cPicks[i].address.city + ', ' + cPicks[i].address.state + ' ' + cPicks[i].address.zipcode + '</p>';
+              phoneHTML = '<p><strong>Phone: </strong>' + cPicks[i].phone + '</p>';
+              foodScoreHTML = '<p><strong>Food Score: </strong>' + ' ' + cPicks[i].score.foodScore + '</p>';
+              drinkScoreHTML = '<p><strong>Drink Score: </strong>' + ' ' + cPicks[i].score.drinkScore + '</p>';
+              ambienceScoreHTML = '<p><strong>Ambience Score: </strong>' + ' ' + cPicks[i].score.ambienceScore + '</p>';
+              thoughtHTML = '<p><strong>My thoughts: </strong>' + ' ' + cPicks[i].comment + '</p>';
+              historyHTML = '<p><strong>History: </strong>' + ' ' + cPicks[i].history + '</p>';
+              $('#title').html(titleHTML);
+              $('#address').html(addressHTML);
+              $('#phone').html(phoneHTML);
+              $('#food-score').html(foodScoreHTML);
+              $('#drink-score').html(drinkScoreHTML);
+              $('#ambience-score').html(ambienceScoreHTML);
+              $('#thought').html(thoughtHTML);
+              $('#history').html(historyHTML);
+            };
+          }
         });
         bounds.extend(markers[i].position);
+        // Push location details to sidebar
       };
 
-      function populateInfoWindow(marker, infowindow) {
-        // Check to see if window is already open
-        if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
-          infowindow.open(map,marker);
-          // Clear marker on close
-          infowindow.addListener('closeclick', function() {
-            infowindow.setMarker = null;
-          });
-        }
-      }
+      // function populateInfoWindow(marker, infowindow) {
+      //   // Check to see if window is already open
+      //   if (infowindow.marker != marker) {
+      //     infowindow.marker = marker;
+      //     infowindow.setContent('<div>' + marker.title + '</div>');
+      //     infowindow.open(map,marker);
+      //     // Clear marker on close
+      //     infowindow.addListener('closeclick', function() {
+      //       infowindow.setMarker = null;
+      //     });
+      //   }
+      // }
 
     }); // End getJson
     
@@ -101,7 +121,7 @@ function initMap() {
       resetMarkers(markers);
     });
 
-
+    // Resize map
     google.maps.event.addDomListener(window, "resize", function() {
      var center = map.getCenter();
      google.maps.event.trigger(map, "resize");
