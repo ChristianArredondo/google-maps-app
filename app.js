ea39a07a6c3f4673f9c9130641180ce50fa2e385
Christian Arredondo <christianarredondo@utexas.edu>
2017-06-18 17:25:19 -0500

    Change map style and add CSS active/inactive classes to buttons

--- app.js
+++ app.js
@@ -2,18 +2,33 @@ var map;
 var markers = [];
 var cPicks;
 var length;
+var sidebarIDs = [];
+
+// // Create array from sidebar div' children' IDs (***For later use once sidebar has Bootstrap divs.)
+// var IDconstructor = (function() {
+//   console.log('Start new function');
+//   childrenLength = $('#sidebar').children().length;
+//   for (i = 0; i < childrenLength; i++) {
+    
+//     newID = $('#sidebar').children()[i].id
+//     sidebarIDs.push(newID)
+//   }
+// }());
+// console.log(sidebarIDs);
 
 function initMap() {
+  //  Define map styles
+  styles = [{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#6600ff"},{"saturation":-11},{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"saturation":"33"},{"hue":"#6600ff"},{"lightness":"2"},{"visibility":"simplified"}]},{"featureType":"poi.attraction","elementType":"all","stylers":[{"saturation":"-58"},{"lightness":"28"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"saturation":"-58"},{"lightness":"28"}]},{"featureType":"poi.government","elementType":"all","stylers":[{"saturation":"-58"},{"lightness":"28"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"saturation":"-58"},{"lightness":"28"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"},{"saturation":"-58"},{"lightness":"28"}]},{"featureType":"poi.place_of_worship","elementType":"all","stylers":[{"saturation":"-58"},{"lightness":"28"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"saturation":"-58"},{"lightness":"28"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"saturation":"-58"},{"lightness":"28"}]},{"featureType":"road","elementType":"all","stylers":[{"hue":"#5e00ff"},{"saturation":-79}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"all","stylers":[{"lightness":30},{"weight":1.3}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"simplified"},{"hue":"#5e00ff"},{"saturation":-16}]},{"featureType":"transit.line","elementType":"all","stylers":[{"saturation":-72}]},{"featureType":"water","elementType":"all","stylers":[{"saturation":-65},{"hue":"#1900ff"},{"lightness":8}]}];
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 29.759248, lng: -95.454905},
-    zoom: 12
+    zoom: 12,
+    styles: styles
   });
 
   // Use AJAX to pull location JSON
   $.getJSON('locations.json', function(response) {
       cPicks=response;
-      console.log(cPicks);
       length = Object.keys(cPicks).length;
       console.log('length = ' + length);
       
@@ -51,6 +66,7 @@ function initMap() {
               ambienceScoreHTML = '<p><strong>Ambience Score: </strong>' + ' ' + cPicks[i].score.ambienceScore + '</p>';
               thoughtHTML = '<p><strong>My thoughts: </strong>' + ' ' + cPicks[i].comment + '</p>';
               historyHTML = '<p><strong>History: </strong>' + ' ' + cPicks[i].history + '</p>';
+              // ***Once Bootstrap is set in sidebar, the sidebarID array will be used in a loop here to push the HTML, as opposed to the repetitive actions below.
               if (titleHTML === '') {
                 // Fade in if first-time click
                 $('#title').html(titleHTML).fadeIn(100);
@@ -96,7 +112,7 @@ function initMap() {
       };
     }); // End getJson
     
-    // Function to reset markers for use on click
+    
     function resetMarkers(markers) {
       for (var i in markers) {
         markers[i].setMap(map);
@@ -104,6 +120,22 @@ function initMap() {
       };
     };
 
+    // // One function to filter all markers for use on click (***For later optimization)
+    // $('.filter').click(function() {
+    //   console.log('Filter by ' + this.val());
+    //   resetMarkers(markers);
+    //   for (var i = 0; i < length; i++) {
+    //     if (cPicks[i].type !== "food" && cPicks[i].type !== "food-bar") {
+    //       markers[i].setMap(null);
+    //     }
+    //   }
+    // });
+
+    // Show All
+      $('#all').click(function() {
+        console.log('Show all!');
+        resetMarkers(markers);
+      });
     // Filter by Food
     $('#food').click(function() {
       console.log('Clicked food filter!');
@@ -134,10 +166,13 @@ function initMap() {
         }
       }
     });
-    // Show All
-    $('#all').click(function() {
-      console.log('Show all!');
-      resetMarkers(markers);
+    // Change button active class on click
+    $('button').click(function() {
+      if ($(this).hasClass('inactive')) {
+        $('button').removeClass('active');
+        $('button').addClass('inactive');
+        $(this).addClass('active');
+      }
     });
 
     // Center map on resize
